190e40d18564c81e99836f9997a221ad93461703
Aaliyan Khan <aaliyank552@gmail.com>
2025-11-11 18:52:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let num = Number(prompt("Enter a number for its table:"));
+let table = document.getElementById("Table");
+table.innerHTML = "<h3> Table of " + num + ":</h3>"
+for (let i = 1; i <= 10; i++) {
+    table.innerHTML += num + " x " + i + " = " + (num * i) + "<br>"
+
+}
