0bcfe7700e8301254d0205a6e0a96056f02df86d
Aaliyan Khan <aaliyank552@gmail.com>
2025-12-20 02:55:18 +0500

    Initial project setup and updates

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let num = Number(prompt("Enter a number for its table:"));
 let table = document.getElementById("Table");
 table.innerHTML = "<h3> Table of " + num + ":</h3>"
 for (let i = 1; i <= 10; i++) {
-    table.innerHTML += num + " x " + i + " = " + (num * i) + "<br>"
+    table.innerHTML += num +" " + " x " +" " + i +" " + " = " +" "+ (num * i) + "<br>"
 
 }
